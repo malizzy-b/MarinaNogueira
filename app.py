ad8cb51bf0b4fe50a2d8176f476a59c3c346bde0
malizzy-b <92615212+malizzy-b@users.noreply.github.com>
2023-11-27 07:28:43 -0300

    trampo

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Variáveis para armazenar as senhas
+senhas_atendimento = []
+senhas_consulta = []
+senhas_chamadas = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', senhas_atendimento=senhas_atendimento, senhas_consulta=senhas_consulta, senhas_chamadas=senhas_chamadas)
+
+def chamar_atendimento():
+    senha = request.form['senha_atendimento']
+    senhas_chamadas.append(senha)
+    return 'Chamada realizada com sucesso'
+
+@app.route('/chamar_consulta', methods=['POST'])
+def chamar_consulta():
+    senha = request.form['senha_consulta']
+    senhas_chamadas.append(senha)
+    return 'Chamada realizada com sucesso'
+
+@app.route('/gerar_senha', methods=['POST'])
+def gerar_senha():
+    tipo = request.form['tipo_senha']
+    if tipo == 'normal':
+        nova_senha = 'B{:03d}'.format(len(senhas_atendimento) + 1)
+        senhas_atendimento.append(nova_senha)
+    elif tipo == 'preferencial':
+        nova_senha = 'A{:03d}'.format(len(senhas_consulta) + 1)
+        senhas_consulta.append(nova_senha)
+
+    # Adiciona a senha gerada à lista
+   
+
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ nodeapp/myapp/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*"],
+  plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+  theme: {
+    extend: {
+      colors: {
+        background: "#4277FF";
+        title: "#0099BA";
+        lightred: "#FF4B4B";
+        lightorange: "#FFA14A";
+        darkblue: "#16009E";
+        purple: "#5200FF";
+        pink: "#FF008A";
+        limegreen: "#85FF91";
+        orange: "#FF9900";
+        white: "#fff";
+        black: "#000";
+      };
+    };
+    fontFamily: {
+      font: ["Mukta", "sans-serif"];
+    };
+  };
+
+
+
